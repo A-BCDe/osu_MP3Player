1ad4cb9f352c010fbf7f6d0eec76f2126b95cce0
A BCDe <a_bcd-e@naver.com>
2017-07-20 18:47:48 +0900

    Start

---
+++ osu! mp3player/osu! mp3player/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace osu__mp3player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindow gameWindow = new GameWindow(640, 480);
+            gameWindow.Run();
+        }
+    }
+}
